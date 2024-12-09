408b2e46d6297af5b9a2497dedf8c70553caf08a
Max Chaplia <82676425+X13X666X@users.noreply.github.com>
2024-12-09 02:06:07 +0200

    Update 5.5.cpp

--- lab5.5/5.5.cpp
+++ lab5.5/5.5.cpp
@@ -1,50 +1,28 @@
 #include <iostream>
 #include <cmath>
 
-int recursionDepth = 0;
-int maxRecursionDepth = 0;
+double pow(double x, int n, int depth = 0) {
+    std::cout << "Recursion depth: " << depth << ", calculating pow(" << x << ", " << n << ")\n";
 
-double positivePower(double x, int n) {
-    recursionDepth++;
-    if (recursionDepth > maxRecursionDepth) {
-        maxRecursionDepth = recursionDepth;
-    }
-
-    double result;
-    if (n == 0) {
-        result = 1;
-    }
-    else {
-        result = x * positivePower(x, n - 1);
-    }
-
-    recursionDepth--;
-    return result;
-}
-
-double power(double x, int n) {
-    if (n < 0) {
-        return 1 / positivePower(x, -n);
-    }
-    return positivePower(x, n);
-}
-
-
-int getMaxRecursionDepth() {
-    return maxRecursionDepth;
+    if (n == 0)
+        return 1;
+    else if (n < 0)
+        return 1 / pow(x, abs(n), depth + 1);
+    else
+        return x * pow(x, n - 1, depth + 1);
 }
 
 int main() {
     double x;
     int n;
+
     std::cout << "Enter x: ";
     std::cin >> x;
     std::cout << "Enter n: ";
     std::cin >> n;
 
-    double result = power(x, n);
+    double result = pow(x, n);
     std::cout << "Result: " << result << std::endl;
-    std::cout << "Max recursion depth: " << getMaxRecursionDepth() << std::endl;
 
     return 0;
 }
